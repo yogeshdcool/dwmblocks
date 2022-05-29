54e9b86ecba09271a827451e8d9cde399ae1748e
yogeshdcool <dyogesh.official@gmail.com>
2022-05-29 22:41:48 +0530

    working

--- blocks.def.h
+++ blocks.def.h
@@ -1,12 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"  ", "~/.config/dwmblocks/scripts/battery",	50,		0},
-	{" ", "~/.config/dwmblocks/scripts/date",	 0,		0},
-	{" ", "~/.config/dwmblocks/scripts/time",	50,		0}
+    { "",           "~/.config/dwmblocks/scripts/sb-datetime",          10,         0       },
+    { "",           "~/.config/dwmblocks/scripts/sb-volume",            0,          10      },
+    { "",           "~/.config/dwmblocks/scripts/sb-battery",           60,         0       },
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
-static unsigned int delimLen = 5;
+static char delim[] = "  ";
+static unsigned int delimLen = 4;
 
