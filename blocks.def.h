2791a91a0b8d3ab8ae1a5a691b0ce1cd8fab746e
yogeshdcool <dyogesh.official@gmail.com>
2022-05-29 23:28:58 +0530

    fixed datetime

--- blocks.def.h
+++ blocks.def.h
@@ -1,7 +1,8 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    { "",           "~/.config/dwmblocks/scripts/sb-datetime",          10,         0       },
+    { "",           "~/.config/dwmblocks/scripts/sb-time",          10,         0       },
+    { "",           "~/.config/dwmblocks/scripts/sb-date",          10,         0       },
     { "",           "~/.config/dwmblocks/scripts/sb-volume",            0,          10      },
     { "",           "~/.config/dwmblocks/scripts/sb-battery",           60,         0       },
 };
